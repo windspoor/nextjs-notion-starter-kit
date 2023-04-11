8f49ba814fd15948fba183bdd414c7762e6119d6
windspoor <82887227+windspoor@users.noreply.github.com>
2023-04-11 16:48:27 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Coffee Home',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'coffeehome.vercel.app',
   author: 'kris',
 
   // open graph metadata (optional)
