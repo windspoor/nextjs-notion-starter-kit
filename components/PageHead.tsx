83ab2c0e1e3686b32333cbfdd9b66b148d2bbca7
windspoor <82887227+windspoor@users.noreply.github.com>
2024-09-05 16:54:43 +0800

    Update PageHead.tsx

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -22,7 +22,6 @@ export const PageHead: React.FC<
 
   return (
     <Head>
-       {/* Google tag (gtag.js) */}
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-4QE697S5R8"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
